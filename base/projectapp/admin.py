0cb3cdbc2d6bddc1f168df4abe47b045aa36f2f6
Hosen MD Altaf <altafhmeazee1999@outlook.com>
2022-08-13 17:51:22 +0600

    project admin updated

--- base/projectapp/admin.py
+++ base/projectapp/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from projectapp.models import Category,Project,ProjectImages
 
+from mptt.admin import MPTTModelAdmin
+
 # Register your models here.
 
-admin.site.register(Category)
+admin.site.register(Category, MPTTModelAdmin)
 
-admin.site.register(Project)
+
+class Projectlist(admin.ModelAdmin):
+    list_display = ('name','categories','image_tag','project_status','location')
+admin.site.register(Project,Projectlist)
 
